594238255aa749527e7eda62a0af14aa5177c5da
AshishManer <ashishmaner94@gmail.com>
2022-12-04 12:50:22 +0530

    first commit

---
+++ middleware/index.js
@@ -0,0 +1,11 @@
+// var rateLimit = require('express-rate-limit')
+
+// const loginAccountLimiter = rateLimit({
+//   windowMS: 10000 * 144 * 60,
+//   max: 5,
+//   Message: 'Too many request made please try after 24hr',
+//   standardHeaders: true,
+//   legacyHeaders: false,
+// })
+
+// module.exports = { loginAccountLimiter }
